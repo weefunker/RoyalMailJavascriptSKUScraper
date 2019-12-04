301cfb3a815d6606fddb085ecf12d5f18361278c
Alex Overend <weefunker@yahoo.co.uk>
2019-12-04 23:37:08 +0000

    Update RoyalMailJavascriptSKUScraper.js

--- RoyalMailJavascriptSKUScraper.js
+++ RoyalMailJavascriptSKUScraper.js
@@ -1,16 +1,20 @@
 var textNodes = document.getElementsByClassName("ellipsis");
+var amazonTotes = document.getElementsByClassName("channel-amazon").length;
+var ebayTotes = document.getElementsByClassName("channel-ebay").length;
+var etsyTotes = document.getElementsByClassName("channel-etsy").length;
+var multiBuyTote = totes - (amazonTotes + etsyTotes + ebayTotes)
 var finalSkuCode = '';
+var totes = textNodes.length
 for (var r = 0; r < textNodes.length; r++) {
 
 var nodeElement = textNodes[r];
 nodeElement = nodeElement.childNodes[2].nodeValue
 
-if (nodeElement.indexOf('a4-') > -1) {
-nodeElement = nodeElement.split('-')[1];
-}
 console.log(nodeElement);
 
 finalSkuCode += nodeElement+"\n";
+console.log(totes)
 
 }
 copy(finalSkuCode);
+`Total posters ${totes} - Amazon ${amazonTotes} - Etsy ${etsyTotes} - Ebay ${ebayTotes} - Total multibuy orders ${multiBuyTote}`
